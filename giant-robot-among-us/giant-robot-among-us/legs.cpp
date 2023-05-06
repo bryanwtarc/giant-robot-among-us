8839272b7e88926d1f1bd3241cf17c8538638559
rtcmb <ryantcm-wm21@student.tarc.edu.my>
2023-05-06 23:21:32 +0800

    Feet done

--- giant-robot-among-us/giant-robot-among-us/legs.cpp
+++ giant-robot-among-us/giant-robot-among-us/legs.cpp
@@ -13,35 +13,104 @@ void Legs::gundamLegs() {
 	glPushMatrix();
 		glBegin(GL_QUADS);
 		//face 1
-		glVertex3f(0.1, 0, -0.15);
-		glVertex3f(0.1, 0, -0.2);
-		glVertex3f(0.05, 0,  -0.2);
-		glVertex3f(0.05, 0, -0.15);
+		glVertex3f(0.1, -0.15, 0);
+		glVertex3f(0.1, -0.2, 0);
+		glVertex3f(0.05, -0.2, 0);
+		glVertex3f(0.05, -0.15, 0);
 		//face 2
-		glVertex3f(0.05, 0, -0.15);
-		glVertex3f(0.1, 0, -0.15);
-		glVertex3f(0.1, 0.025, -0.15);
-		glVertex3f(0.05, 0.025, -0.15);
+		glVertex3f(0.05, -0.15, 0);
+		glVertex3f(0.1, -0.15, 0);
+		glVertex3f(0.1, -0.15, 0.025);
+		glVertex3f(0.05, -0.15, 0.025);
 		//face 3
-		glVertex3f(0.05, 0.025, -0.15);
-		glVertex3f(0.05, 0.025, -0.2);
-		glVertex3f(0.1, 0.025, -0.2);
-		glVertex3f(0.1, 0.025, -0.15);
+		glVertex3f(0.05, -0.15, 0.025);
+		glVertex3f(0.05, -0.2, 0.025);
+		glVertex3f(0.1, -0.2, 0.025);
+		glVertex3f(0.1, -0.15, 0.025);
 		//face 4
-		glVertex3f(0.1, 0.025, -0.15);
-		glVertex3f(0.1, 0, -0.15);
-		glVertex3f(0.05, 0, -0.15);
-		glVertex3f(0.05, 0.025, -0.15);
+		glVertex3f(0.1, -0.15, 0.025);
+		glVertex3f(0.1, -0.15, 0);
+		glVertex3f(0.05, -0.15, 0);
+		glVertex3f(0.05, -0.15, 0.025);
 		//face 5
-		glVertex3f(0.05, 0.025, -0.15);
-		glVertex3f(0.05, 0, -0.15);
-		glVertex3f(0.05, 0, -0.2);
-		glVertex3f(0.05, 0.025, -0.2);
+		glVertex3f(0.05, -0.15, 0.025);
+		glVertex3f(0.05, -0.15, 0);
+		glVertex3f(0.05, -0.2, 0);
+		glVertex3f(0.05, -0.2, 0.025);
 		//face 6
-		glVertex3f(0.1, 0.025, -0.15);
-		glVertex3f(0.1, 0.025, -0.15);
-		glVertex3f(0.1, 0, -0.15);
-		glVertex3f(0.05,0 , -0.15);
+		glVertex3f(0.1, -0.15, 0.025);
+		glVertex3f(0.1, -0.15, 0.025);
+		glVertex3f(0.1, -0.15, 0);
+		glVertex3f(0.05, -0.15, 0);
 		glEnd();
-	glPopMatrix();
+		
+		//SOLE OF FEET
+		glBegin(GL_QUADS);
+		//face 1
+		glVertex3f(0.025,  -0.2, 0.025);
+		glVertex3f(0.025,  -0.15, 0.025);
+		glVertex3f(0.025, -0.15, 0);
+		glVertex3f(0.025, -0.2, 0);
+		//face 2
+		glVertex3f(0.025, -0.2, 0);
+		glVertex3f(0, -0.2, 0);
+		glVertex3f(0, -0.2, 0.025);
+		glVertex3f(0.025, -0.2, 0.025);
+		//face 3
+		glVertex3f(0.025, -0.2, 0.025);
+		glVertex3f(0.025, -0.15, 0.025);
+		glVertex3f(0, -0.15, 0.025);
+		glVertex3f(0, -0.2, 0.025);
+		//face 4
+		glVertex3f(0, -0.2, 0.025);
+		glVertex3f(0, -0.2, 0);
+		glVertex3f(0, -0.15, 0);
+		glVertex3f(0, -0.15, 0.025);
+		//face 5
+		glVertex3f(0, -0.15, 0.025);
+		glVertex3f(0.025, -0.15, 0.025);
+		glVertex3f(0.025, -0.15, 0);
+		glVertex3f(0, -0.15, 0);
+		//face 6
+		glVertex3f(0, -0.15, 0);
+		glVertex3f(0, -0.2, 0);
+		glVertex3f(0.025, -0.2, 0);
+		glVertex3f(0.025, -0.15, 0);
+		glEnd();
+
+		//Face of feet
+		glBegin(GL_POLYGON);
+		//face 1
+		glVertex3f(0.1, -0.2, 0.025);
+		glVertex3f(0.1, -0.15, 0.025);
+		glVertex3f(0, -0.15, 0.025);
+		glVertex3f(0, -0.2, 0.025);
+		//face 2
+		glVertex3f(0, -0.2, 0.025);
+		glVertex3f(0.025, -0.2, 0.06);
+		glVertex3f(0.025, -0.15, 0.06);
+		glVertex3f(0, -0.15, 0.025);
+		//face 3
+		glVertex3f(0, -0.15, 0.025);
+		glVertex3f(0.1, -0.15, 0.025);
+		glVertex3f(0.05, -0.15, 0.06);
+		glVertex3f(0.025, -0.15, 0.06);
+		//face 4
+		glVertex3f(0.025, -0.15, 0.06);
+		glVertex3f(0.025, -0.2, 0.06);
+		glVertex3f(0.05, -0.2, 0.06);
+		glVertex3f(0.05, -0.15, 0.06);
+		//face 5
+		glVertex3f(0.05, -0.15, 0.06);
+		glVertex3f(0.1, -0.15, 0.025);
+		glVertex3f(0.1, -0.2, 0.025);
+		glVertex3f(0.05, -0.2, 0.06);
+		//face 6
+		glVertex3f(0.05, -0.2, 0.06);
+		glVertex3f(0.025, -0.2, 0.06);
+		glVertex3f(0, -0.2, 0.025);
+		glVertex3f(0.1, -0.2, 0.025);
+		glEnd();
+
+		glPopMatrix();
 }
