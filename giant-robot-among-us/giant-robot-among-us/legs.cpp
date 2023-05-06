e0a2c3acdfb19097ea1a1d647156e9e97e97f298
rtcmb <ryantcm-wm21@student.tarc.edu.my>
2023-05-06 20:31:41 +0800

    Added Legs

--- giant-robot-among-us/giant-robot-among-us/legs.cpp
+++ giant-robot-among-us/giant-robot-among-us/legs.cpp
@@ -1,1 +1,38 @@
-#include "legs.h"
+#include <windows.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include "DrawFunctions.h"
+#include "Legs.h"
+
+void Legs::legs() {
+	glClearColor(0, 0, 0, 0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+	//feet
+	glScalef(3.0, 3.0, 3.0);
+	glPushMatrix();
+		glBegin(GL_QUADS);
+		//face 1
+		glVertex3f(0.1, 0, -0.15);
+		glVertex3f(0.1, 0, -0.2);
+		glVertex3f(0.05, 0,  -0.2);
+		glVertex3f(0.05, 0, -0.15);
+		//face 2
+		glVertex3f(0.05, 0, -0.15);
+		glVertex3f(0.1, 0, -0.15);
+		glVertex3f(0.1, 0.025, -0.15);
+		glVertex3f(0.05, 0.025, -0.15);
+		//face 3
+		glVertex3f(0.05, 0.025, -0.15);
+		glVertex3f(0.05, 0.025, -0.2);
+		glVertex3f();
+		glVertex3f();
+		glEnd();
+	glPopMatrix();
+
+}
--- giant-robot-among-us/giant-robot-among-us/legs.h
+++ giant-robot-among-us/giant-robot-among-us/legs.h
@@ -1,29 +1,22 @@
 #pragma once
-#ifndef Legs_h
-#define Legs_h
+#ifndef LEGS_H
+#define LEGS_H
 #endif
 
-#if defined(__APPLE__)
-#include <OpenGL/gl.h>
-#include <OpenGL/glu.h>
-#include <GLUT/glut.h>
-#else
 #include <windows.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
-
-#endif
 #include <iostream>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
-#include "DrawFunctions.h"
+#include "DRAWFUNCTIONS.H"
 #include "DrawFunctions.cpp"
 
-class legs
+class Legs
 {
 public:
-	
+	void legs();
 };
 
