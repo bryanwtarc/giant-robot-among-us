f5508cfee914c69d0ed7e860256af4d138f34c2e
bryanwtarc <bryanwbl-wp20@student.tarc.edu.my>
2023-05-07 18:50:08 +0800

    hand done

--- giant-robot-among-us/giant-robot-among-us/UpperBody.cpp
+++ giant-robot-among-us/giant-robot-among-us/UpperBody.cpp
@@ -5,22 +5,73 @@
 #include "DrawFunctions.h"
 #include "UpperBody.h"
 
+void UpperBody::finger() {
+	glPushMatrix();
+		glPushMatrix();
+			glRotatef(30, 0, 0, 1);
+			glColor3f(0.25, 0.25, 0.25);
+			glScalef(1.2, 1, 1);
+			glTranslatef(-0.05, 0.2, 0);
+			cube(0.2);
+		glPopMatrix();
+
+		glPushMatrix();
+			glRotatef(90, 0, 0, 1);
+			glScalef(1, 1, 0.75);
+			glTranslatef(0, 0, 0.05);
+			cube(0.2);
+		glPopMatrix();
+
+		glPushMatrix();
+			glTranslatef(-0.1, -0.1, 0.05);
+			glScalef(2, 1, 1);
+			cube(0.15);
+		glPopMatrix();
+	glPopMatrix();
+}
+
 void UpperBody::hand() {
 	/* Palm */
 	glPushMatrix();
 		glColor3f(0.5, 0.5, 0.5);
-		glScalef(1, 0.5, 1);
-		glTranslatef(0, 0.5, 0);
+		glScalef(1.25, 0.5, 1.25);
+		glTranslatef(0, 0.5, -0.01);
 		cube(0.5);
 	glPopMatrix();
 	/* ------- */
 
-	/* Index Finger */
+	/* Thumb */
 	glPushMatrix();
-		glColor3f(0.25, 0.25, 0.25);
-		glScalef(1.2, 1, 1);
-		glTranslatef(-0.2, 0.3, 0);
-		cube(0.2);
+		glPushMatrix();
+			glColor3f(0.3, 0.3, 0.3);
+			glTranslatef(0.2, 0.1, -0.2);
+			cube(0.2);	
+		glPopMatrix();
+
+		glPushMatrix();
+			glScalef(0.75, 0.75, 1.5);
+			glRotatef(30, 1, 0, 0);
+			glTranslatef(0.3, -0.1, -0.1);
+			cube(0.2);
+		glPopMatrix();
 
 	glPopMatrix();
+
+	/* Index Finger */
+	glPushMatrix();
+		finger();
+	glPopMatrix();
+
+	/* Middle Finger */
+	glPushMatrix();
+		glTranslatef(0, 0, 0.2);
+		finger();
+	glPopMatrix();
+
+	/* Third Finger */
+	glPushMatrix();
+		glTranslatef(0, 0, 0.4);
+		finger();
+	glPopMatrix();
+
 }
